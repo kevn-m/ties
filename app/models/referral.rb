57973bb6464f0fa99308e1c59659ecae65788298
kjam0 <south.kay@gmail.com>
2021-07-17 12:10:31 +1000

    models completed

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -1,2 +1,6 @@
 class Referral < ApplicationRecord
+  # belongs_to :user
+  belongs_to :referrer_user, class_name: 'User', foreign_key: 'referrer_user_id'
+  belongs_to :recommended_user, class_name: 'User', foreign_key: 'recommended_user_id'
+  belongs_to :to_user, class_name: 'User', foreign_key: 'to_user_id'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :referrals
+  has_many :referrer, foreign_key: 'referrer_user_id', class_name: 'Referral'
+  has_many :recommended, foreign_key: 'recommended_user_id', class_name: 'Referral'
+  has_many :to_referrals, foreign_key: 'to_user_id', class_name: 'Referral'
   has_many :ties
   has_many :user_interests
   has_many :interests, through: :user_interests
   has_many :messages
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :bio, length: { minimum: 50 }
+  validates :summary, length: { maximum: 500 }
+  validates :status, length: { maximum: 100 }
 end
