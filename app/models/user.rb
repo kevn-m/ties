3aed2ec4c2c9185fc3cf4ac7cddf90a2caf50f30
kjam0 <south.kay@gmail.com>
2021-07-17 15:00:25 +1000

    added cloudinary config and active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,6 @@ class User < ApplicationRecord
   validates :bio, length: { minimum: 50 }
   validates :summary, length: { maximum: 500 }
   validates :status, length: { maximum: 100 }
+
+  has_many_attached :photos
 end
