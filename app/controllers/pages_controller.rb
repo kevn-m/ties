a1e1f7cc8b2395caa2a8200e9b2e31b11ac3bad6
Leeten Chin <lchin@imsystems.com.au>
2021-07-20 19:30:08 +1000

    login page fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @resource = User.new()
+    # @resource = User.new()
+    if user_signed_in?
+      redirect_to ties_path
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
