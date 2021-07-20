2dd4bf7a7c779e5efd1b7a9efd1db4c3b1a07ac8
kjam0 <south.kay@gmail.com>
2021-07-20 21:45:40 +1000

    added controller and view for referrals that shows the referred users profile on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,5 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    # Show a list of people who are recommended to the user
-    @recommended_users = Referral.where(to_user_id: current_user)
   end
 end
--- app/controllers/referrals_controller.rb
+++ app/controllers/referrals_controller.rb
@@ -9,6 +9,10 @@ class ReferralsController < ApplicationController
   end
 
   def index
+    @to_referrals = current_user.to_referrals
+    @referrals = @to_referrals.map do |referral|
+      User.find_by id: referral.recommended_user_id
+    end
   end
 
   def create
