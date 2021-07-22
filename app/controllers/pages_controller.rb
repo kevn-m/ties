36849a2e9bd2ca6949daf3e628c8df0606eb3636
Leeten Chin <lchin@imsystems.com.au>
2021-07-22 19:30:08 +1000

    login and signup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
   def home
     # @resource = User.new()
     if user_signed_in?
-      redirect_to ties_path
+      redirect_to referrals_path
     else
       redirect_to new_user_session_path
     end
