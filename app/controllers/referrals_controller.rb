fd287207d091372f8333001c4a0c09f2f60ae70c
kjam0 <south.kay@gmail.com>
2021-07-22 21:51:21 +1000

    added slider.js which is empty right now and updated all the js files to allow for it to export properly. Also added comments of psuedocode for next steps

--- app/controllers/referrals_controller.rb
+++ app/controllers/referrals_controller.rb
@@ -10,17 +10,13 @@ class ReferralsController < ApplicationController
 
   def index
 
-    # getting all referrals for the to_user
-    @to_referrals = current_user.to_referrals
+    # getting all referrals for the to_user where reject = nil
+    @to_referrals = current_user.to_referrals.where(reject: nil)
 
-    # mapping them into an array if the are not rejected and not seen
+    # mapping users recommended users to an array
     @referrals = @to_referrals.map do |referral|
       User.find_by id: referral.recommended_user_id
     end
-
-    # Map all current friends to an array
-
-    #
 
   end
 
