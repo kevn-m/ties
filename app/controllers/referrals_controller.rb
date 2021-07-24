2e5d333113bc1b03e5898bc36afac9996f9a2f97
kjam0 <south.kay@gmail.com>
2021-07-24 16:53:45 +1000

    added to the homepage to display a list of current ties and so we can refer them

--- app/controllers/referrals_controller.rb
+++ app/controllers/referrals_controller.rb
@@ -28,7 +28,19 @@ class ReferralsController < ApplicationController
     @to_referrals = current_user.to_referrals.where(reject: nil).where(requested: nil)
 
     # Get all referrals where the current user has been requested (recommended_user)
-    @requested_referral = current_user.recommended.where(reject: false).where(requested: true)
+    @requested_referrals = current_user.recommended.where(reject: false).where(requested: true)
+
+    @ties = Tie.where('user1_id= :user OR user2_id= :user', {user: current_user})
+    # Looping through the ties for current_user and returning the other user for each tie.
+    @users = []
+    @ties.each do |tie|
+      if current_user.id == tie.user1_id
+        @user_id = tie.user2_id
+      elsif current_user.id == tie.user2_id
+        @user_id = tie.user1_id
+      end
+      @users.push(User.find(@user_id))
+    end
   end
 
   def create
