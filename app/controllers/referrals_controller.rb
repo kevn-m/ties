47bd9424424fc5e3bee33061c1fd54471a44ac98
kjam0 <south.kay@gmail.com>
2021-07-24 12:27:47 +1000

    implemented where user clicks request, updates the referral model to flag the requested = true and reject = nil

--- app/controllers/referrals_controller.rb
+++ app/controllers/referrals_controller.rb
@@ -1,13 +1,13 @@
 class ReferralsController < ApplicationController
   def referral_request
+    user_id = params[:id]
+    @referral = Referral.find_by to_user_id: current_user, recommended_user_id: user_id
+    @referral.update(reject: false, requested: true)
+    redirect_to referrals_path
   end
 
   def accept
-    user_id = params[:id]
-    @referral = Referral.find_by to_user_id: current_user, recommended_user_id: user_id
-    @referral.update(reject: false)
-    Tie.create(user1_id: current_user.id, user2_id: params[:id])
-    redirect_to referrals_path
+    # Tie.create(user1_id: current_user.id, user2_id: params[:id])
   end
 
   def reject
@@ -19,12 +19,15 @@ class ReferralsController < ApplicationController
 
   def index
 
-    # getting all referrals for the to_user where reject = nil
-    @to_referrals = current_user.to_referrals.where(reject: nil)
+    # getting all referrals for the to_user where reject = nil and where requested = nil
+    @to_referrals = current_user.to_referrals.where(reject: nil).where(requested: nil)
 
     # mapping users recommended users to an array
     @referrals = @to_referrals.map do |referral|
       User.find_by id: referral.recommended_user_id
+
+    # Get all referrals where the recommended_user_id is equal to current user AND requested is nil
+
     end
 
   end
