96852697f4b4ab5c9a444c3e68e19741aa14eaed
Talina Bay <talinabayeleva@gmail.com>
2021-07-31 12:09:02 +1000

    updated ties controller

--- app/controllers/ties_controller.rb
+++ app/controllers/ties_controller.rb
@@ -1,5 +1,19 @@
 class TiesController < ApplicationController
   def chat_list
+    @ties = Tie.where('user1_id= :user OR user2_id= :user', { user: current_user })
+    # Looping through the ties for current_user and returning the other user for each tie.
+    @users = []
+
+    @ties.each do |tie|
+      if current_user.id == tie.user1_id
+        @user_id = tie.user2_id
+      elsif current_user.id == tie.user2_id
+        @user_id = tie.user1_id
+      end
+      @tie = tie.id
+      @lastmessage = Message.where(tie_id: tie.id).last
+      @users.push([User.find(@user_id), @lastmessage, @tie])
+    end
   end
 
   def index
@@ -19,5 +33,11 @@ class TiesController < ApplicationController
   def show
     @tie = Tie.find(params[:id])
     @message = Message.new
+    # @message.update(message_params)
   end
+
+  # private
+  # def message_params
+  #   params.require().permit(:user_id, :tie_id, :content, :seen)
+  # end
 end
