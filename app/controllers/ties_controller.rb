a26e3cbaabbc69c0a7b4d3ae255c94a8b49413ab
Hanna Koskela <haleko@yahoo.com>
2021-08-04 19:58:51 +1000

    Added pg_search. User can now search ties based on their interests

--- app/controllers/ties_controller.rb
+++ app/controllers/ties_controller.rb
@@ -20,6 +20,7 @@ class TiesController < ApplicationController
     @ties = Tie.where('user1_id= :user OR user2_id= :user', { user: current_user })
     # Looping through the ties for current_user and returning the other user for each tie.
     @users = []
+    @all_matches = []
     @ties.each do |tie|
       if current_user.id == tie.user1_id
         @user_id = tie.user2_id
@@ -29,6 +30,15 @@ class TiesController < ApplicationController
       @users.push(User.find(@user_id))
       # Making sure that we display each user only once
       @users = @users.uniq
+    end
+
+    if params[:query].present?
+      # First finding all the users that match the search
+      @all_matches = User.search_by_interest(params[:query])
+      # Then getting an intersection (common entries) of @all_matches and @users
+      @users = @users & @all_matches
+    else
+      @users
     end
   end
 
