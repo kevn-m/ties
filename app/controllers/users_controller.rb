a46e6ac52f5cfa557f42ce4dd3257991e3422722
Leeten Chin <lchin@imsystems.com.au>
2021-08-05 18:34:19 +1000

    delete photos works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: [:show, :edit, :update, :update_interests, :update_profile]
+  before_action :find_user, only: [:show, :edit, :update, :update_interests, :update_profile, :update_photos]
   before_action :store_url, only: [:edit_profile]
 
   def show
@@ -59,6 +59,52 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit_photos
+    # binding.pry
+    if user_signed_in?
+      @user = User.find(current_user.id)
+    else
+      redirect_to root_path, notice: "Sorry, you must login first."
+    end
+  end
+
+  def delete_photo
+    # binding.pry
+    if user_signed_in?
+      user = User.find(current_user.id)
+      blob = ActiveStorage::Blob.find_signed(params[:id])
+
+             #binding.pry
+      user.photos.find(blob.id)&.purge  #purge if photo exists using the '&.' as a shot form
+
+      redirect_to edit_photos_path
+    else
+      redirect_to root_path, notice: "Sorry, you must login first."
+    end
+  end
+
+  def update_photos
+    if user_signed_in?
+      # binding.pry
+      if @user.update!(user_params)
+
+        # now add the photos
+
+        params[:user][:photos].each do |pic|
+          @user.photos.attach(io: pic.tempfile, filename: pic.original_filename, content_type: pic.content_type)
+        end
+
+        redirect_to after_update_path_for(@user), notice: "Your interest is updated sucessfully !"
+        # redirect_back(fallback_location: root_path)
+      else
+        flash.now[:alert] = @user.errors.full_messages.first
+        # render :edit_interests
+      end
+
+    else
+      redirect_to root_path, notice: "Sorry, you must login first."
+    end
+  end
   private
 
   def user_params
