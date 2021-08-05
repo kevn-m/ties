77103b78c905234cee2e9a9dac7f9baa78d5bb34
Hanna Koskela <haleko@yahoo.com>
2021-08-05 21:21:29 +1000

    Added javascript to stop New Referral page from scrolling to top after selecting a user

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,10 +25,34 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+Turbolinks.scroll = {};
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 
   initProfileView();
+
+  // below has been added to prevent the page scrolling to the top on
+  // New Referral page after clicking "Select"
+  const elements = document.querySelectorAll("[data-turbolinks-scroll]");
+
+  elements.forEach(function(element){
+
+    element.addEventListener("click", ()=> {
+      Turbolinks.scroll['top'] = document.scrollingElement.scrollTop;
+    });
+
+    element.addEventListener("submit", ()=> {
+      Turbolinks.scroll['top'] = document.scrollingElement.scrollTop;
+    });
+
+  });
+
+  if (Turbolinks.scroll['top']) {
+    document.scrollingElement.scrollTo(0, Turbolinks.scroll['top']);
+  }
+
+  Turbolinks.scroll = {};
 });
+
